264087d15662a59bad0b632664202cd8f2735596
Kosuke Aoki <mail@noraworld.com>
2022-10-03 11:23:19 +0900

    Delete iptables/youtube/

--- iptables/youtube/insert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo iptables -w -N YOUTUBE
-sudo iptables -w -A YOUTUBE -j REJECT
-
-sudo iptables -w -I OUTPUT  -p udp -m string --string "youtube"     --algo bm --from 1 --to 600 -j YOUTUBE
-sudo iptables -w -I FORWARD -p udp -m string --string "youtube"     --algo bm --from 1 --to 600 -j YOUTUBE
-sudo iptables -w -I OUTPUT  -p udp -m string --string "googlevideo" --algo bm --from 1 --to 600 -j YOUTUBE
-sudo iptables -w -I FORWARD -p udp -m string --string "googlevideo" --algo bm --from 1 --to 600 -j YOUTUBE
